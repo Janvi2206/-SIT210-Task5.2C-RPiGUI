8e9cc52e2691d33eb5264be5aa64bb5eaf89a010
Janvi2206 <133149556+Janvi2206@users.noreply.github.com>
2023-09-13 10:18:53 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -51,12 +51,12 @@ def ledAllToggle():
      
 
 ###WIDGETS###
-ledRedButton1 = Button(win,  text = 'Turn Red LED On', font= myFont, command=ledToggle,  bg= 'bisque2', height =1, width=24)
+ledRedButton1 = Button(win,  text = 'Turn Red LED On', font= myFont, command=ledRedToggle,  bg= 'bisque2', height =1, width=24)
 ledRedButton1.grid(row=0, column=1)
-ledBlueButton2 = Button(win,  text = 'Turn White LED On', font= myFont, command=ledToggle,  bg= 'bisque2', height =1, width=24)
+ledBlueButton2 = Button(win,  text = 'Turn White LED On', font= myFont, command=ledBlueToggle,  bg= 'bisque2', height =1, width=24)
 ledBlueButton2.grid(row=0, column=2)
-ledGreenButton3 = Button(win,  text = 'Turn Green LED On', font= myFont, command=ledToggle,  bg= 'bisque2', height =1, width=24)
+ledGreenButton3 = Button(win,  text = 'Turn Green LED On', font= myFont, command=ledGreenToggle,  bg= 'bisque2', height =1, width=24)
 ledGreenButton3.grid(row=0, column=3)
-ledAllButton4= Button(win,  text = 'Turn All LED On', font= myFont, command=ledToggle,  bg= 'bisque2', height =1, width=24)
+ledAllButton4= Button(win,  text = 'Turn All LED On', font= myFont, command=ledAllToggle,  bg= 'bisque2', height =1, width=24)
 ledAllButton4.grid(row=0, column=4)
  

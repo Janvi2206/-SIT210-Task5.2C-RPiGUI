9ad7b05d3b0303dded0257a26eda03633a798a99
Janvi2206 <133149556+Janvi2206@users.noreply.github.com>
2023-09-18 18:43:34 +0530

    GUI.py

--- GUI.py
+++ GUI.py
@@ -41,22 +41,19 @@ def ledAllToggle():
           ledRed.off()
           ledBlue.off()
           ledGreen.off()
-          ledAllButton4['text']="Turn All LED on"
-     
+          ledAllButton4['text']="Turn All LED on"     
      else:
           ledRed.on()
           ledBlue.on()
           ledGreen.on()     
-          ledAllButton4['text']="Turn All LED off"
-     
+          ledAllButton4['text']="Turn All LED off"   
 
 ###WIDGETS###
 ledRedButton1 = Button(win,  text = 'Turn Red LED On', font= myFont, command=ledRedToggle,  bg= 'tomato', height =1, width=24)
-ledRedButton1.grid(row=0, column=1)
+ledRedButton1.grid(row=1, column=0)
 ledBlueButton2 = Button(win,  text = 'Turn White LED On', font= myFont, command=ledBlueToggle,  bg= 'aquamarine', height =1, width=24)
-ledBlueButton2.grid(row=0, column=2)
+ledBlueButton2.grid(row=2, column=0)
 ledGreenButton3 = Button(win,  text = 'Turn Green LED On', font= myFont, command=ledGreenToggle,  bg= 'green yellow', height =1, width=24)
-ledGreenButton3.grid(row=0, column=3)
+ledGreenButton3.grid(row=3, column=0)
 ledAllButton4= Button(win,  text = 'Turn All LED On', font= myFont, command=ledAllToggle,  bg= 'bisque2', height =1, width=24)
-ledAllButton4.grid(row=0, column=4)
- 
+ledAllButton4.grid(row=4, column=0)

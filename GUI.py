87a8530c7bd2fd57ae3fc777c1e2811cf340b407
Janvi2206 <133149556+Janvi2206@users.noreply.github.com>
2023-10-22 15:52:17 +0530

    GUI.py

--- GUI.py
+++ GUI.py
@@ -5,55 +5,38 @@ import RPi.GPIO
 RPi.GPIO.setmode(RPi.GPIO.BCM)
 
 ## hardware 
-ledRed = LED(14)
-ledBlue = LED(15)
-ledGreen = LED(18)
-
+leds = {
+     "Red": LED(14),
+     "Blue": LED(15),
+     "Green": LED(18),
+}
+     
 ## GUI DEFINITIONS ##
 win = Tk()
 win.title("LED Toggler")
 myFont = tkinter.font.Font(family='Helvetica', size=12, weight="bold")
 
 ### EVENT FUNCTIONS ###
-def ledRedToggle():
-     if ledRed.is_lit:
-          ledRed.off()
-          ledRedButton1["text"] = "Turn Red LED on"
+def ledToggle(led):
+     if led.is_lit:
+          led.off()
      else:
           ledRed.on()
-          ledRedButton1['text'] = "Turn Red LED off"
-def ledBlueToggle():
-     if ledBlue.is_lit:
-          ledBlue.off()
-          ledBlueButton2["text"] = "Turn Blue LED on"
-     else:
-          ledBlue.on()
-          ledBlueButton2['text'] = "Turn Blue LED off"
-def ledGreenToggle():
-     if ledGreen.is_lit:
-          ledGreen.off()
-          ledGreenButton3["text"] = "Turn Green LED on"
-     else:
-          ledGreen.on()
-          ledGreenButton3['text'] = "Turn Green LED off"
+##WIDGETS##
+buttons = []
+for name, led in leds.items():
+     button = Button(win, text=f'Turn {name} LED On', font=myFont, command=lambda l=led: led_toggle(1), height=1, width=24)
+     button.grid(row=len(buttons) +1, column=0)
+     buttons.append(button)
+     
 def ledAllToggle():
-     if ledRed.is_lit & ledBlue.is_lit & ledGreen.is_lit:
-          ledRed.off()
-          ledBlue.off()
-          ledGreen.off()
-          ledAllButton4['text']="Turn All LED on"     
-     else:
-          ledRed.on()
-          ledBlue.on()
-          ledGreen.on()     
-          ledAllButton4['text']="Turn All LED off"   
+     all_leds_on = all(led.is_lit for led in leds.values())
+     for led in leds.values():
+        if all_leds_on:
+            led.off()
+        else:
+            led.on()   
 
 ###WIDGETS###
-ledRedButton1 = Button(win,  text = 'Turn Red LED On', font= myFont, command=ledRedToggle,  bg= 'tomato', height =1, width=24)
-ledRedButton1.grid(row=1, column=0)
-ledBlueButton2 = Button(win,  text = 'Turn White LED On', font= myFont, command=ledBlueToggle,  bg= 'aquamarine', height =1, width=24)
-ledBlueButton2.grid(row=2, column=0)
-ledGreenButton3 = Button(win,  text = 'Turn Green LED On', font= myFont, command=ledGreenToggle,  bg= 'green yellow', height =1, width=24)
-ledGreenButton3.grid(row=3, column=0)
-ledAllButton4= Button(win,  text = 'Turn All LED On', font= myFont, command=ledAllToggle,  bg= 'bisque2', height =1, width=24)
-ledAllButton4.grid(row=4, column=0)
+ledAllButton= Button(win,  text = 'Turn All LED On', font= myFont, command=ledAllToggle,  bg= 'bisque2', height =1, width=24)
+ledAllButton.grid(row=len(leds)+1, column=0)
